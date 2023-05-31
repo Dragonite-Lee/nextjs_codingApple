91954310f111a32bf1487b9c0ff1702091c869c1
dragonite-lee <ross1222@naver.com>
2023-05-31 14:00:54 +0900

    좋아요2 array와 object state 변경까지 완료

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,20 +1,32 @@
 import Hi from './data'
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta'];
   return (
     <div>
       <Hi></Hi>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+        <CartItem item={장바구니[0]} />
+        <CartItem item={장바구니[1]} />
+        <Button color="red"/>
+        <Button color="blue"/>
     </div>
   )
 } 
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
+
+function Button(props) {
+  return (
+    <div>
+      <button style={{backgroundColor: props.color}}>버튼</button>
+    </div>
+  )
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,11 @@
+'use client'
 import Image from "next/image";
 import tomato from '/public/food0.png'
+import { useState } from "react";
 
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
-    상품.map((a)=>{
-      return 'Apples'
-    })//이렇게 return하면 return값을 추가해서 어레이를 남겨줌 근데, 배열의 수만큼 담기게 됨 Apples, Apples, Apples
+    let [count, setCount] = useState([0, 0, 0]);
     return (
         <div>
         <h2 className="title">Products</h2>
@@ -13,16 +13,21 @@ export default function List() {
           상품.map((data,index) => {
             return (
               <div className="food" key={index}>
-                {/* 일반 이미지 넣는 법*/}
-                {/* 참고로 이미지는 퍼블릭폴더에 저장하며, alt를 이용하면 검색에도 이점이 있음 */}
                 <img src={`/food${index}.png`} className="food-img" />
-                {/* laze loading 사이즈 최적화 layout shift방지등 최적화된 이미지로 넣을 수 있음 */}
-                {/* src주소 import해야함 */}
-                {/* <Image src={tomato} className="food-img" /> */}
-                {/* 만약 외부이미지를 넣으려고 src="https://s3.amazonaws.com/my-bucket/profile.png" 하려면 width, height 속성이 필요함 */}
-                {/* 그리고 next.config.js에서 가져다 쓰겠습니다 하는 도메인 등록, 경로 등록 을 해야한다. */}
-                {/* 그리고 반복문 안에서 다른 이미지를 보여주려면 require('이미지 경로')사용 */}
                 <h4>{data} $40</h4>
+                <span> {count[index]} </span>
+                <button onClick={() => {
+                  let countCopy = [...count]
+                  countCopy[index]++
+                  setCount(countCopy)}
+                  }>+
+                </button>
+                <button onClick={() => {
+                  let countCopy = [...count]
+                  countCopy[index]--
+                  setCount(countCopy)}
+                  }>-
+                </button>
               </div>
             )
           })
